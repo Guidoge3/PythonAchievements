3e497e18299671305048a40afea3882fce9118fe
31888@ma-web.nl <31888@ma-web.nl>
2020-09-18 15:13:48 +0200

    toevoeging Poem Python achievements

---
+++ Poem Python achievements.py
@@ -0,0 +1,8 @@
+versA = "Mijn naam is Guido en ik ben 17 jaar."
+versB = "ik woon in nieuw vennep voor al 2 jaar."
+versC = "ik hou niet van mensen die zich anders voordoen dan ze echt zijn."
+versD = "ik hou van games die leuk zijn maar geven vaak ook brein pijn."
+versE = "ik vind het leuk om games te spelen met veel vrienden."
+versF = "alleen is het niet altijd makkelijk mensen te vinden."
+
+print(versA, versB, versC, versD, versE, versF)
