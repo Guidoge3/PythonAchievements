a724c36b325ac7cde00c40ca0c91a40a124638ad
31888@ma-web.nl <31888@ma-web.nl>
2020-10-02 15:15:45 +0200

    Nieuwe commit Game character

---
+++ Game Character.py
@@ -0,0 +1,49 @@
+import time
+
+
+print("welcome to the game, this is your character:")
+
+
+A = True
+B = False
+CName = "Harold"
+speed = 1485
+strength = 80
+intelligence = 107
+Power = "Power Punch"
+Level = 69
+JumpForce = 10
+ammo = 42069
+slots = 6
+Friends = 0
+Weight = 90
+Wanted = True
+
+print("this is your Character:")
+
+time.sleep ( 2 )
+
+print("Name:", CName)
+time.sleep ( 1 )
+print("Speed:", speed)
+time.sleep ( 1 )
+print("Strength:", strength)
+time.sleep ( 1 )
+print("Level:", Level)
+time.sleep ( 1 )
+print("Power:", Power)
+time.sleep ( 1 )
+print("Intelligence:", intelligence)
+time.sleep ( 1 )
+print("JumpForce:", JumpForce)
+time.sleep ( 1 )
+print("Ammo:", ammo)
+time.sleep ( 1 )
+print("InventorySlots:", slots)
+time.sleep ( 1 )
+print("Friends:", Friends)
+time.sleep ( 1 )
+print("Weight:", Weight)
+time.sleep ( 1 )
+print("Wanted:", Wanted)
+time.sleep ( 1 )
