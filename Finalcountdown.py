c0689f27e442f9944bf88a565ddf6c9c5e52a73b
31888@ma-web.nl <31888@ma-web.nl>
2020-10-30 15:17:55 +0100

    final countdown toegevoegd

---
+++ Finalcountdown.py
@@ -0,0 +1,23 @@
+import time
+import webbrowser
+
+print("is the final countdown!")
+time.sleep(1)
+
+for x in reversed(range(0, 1001)):
+    print(x)
+    
+time.sleep(5)
+
+webbrowser.open('https://www.youtube.com/watch?v=C_bP_gBK3nk', new=0)
+print("Sefa = sick!")
+
+time.sleep(5)
+
+for y in reversed(range(0, 10001)):
+    print(y)
+
+time.sleep(20)
+
+webbrowser.open('https://www.youtube.com/watch?v=9jK-NcRmVcw', new=1)
+print("its the final countdown! tu du du duhhh")
